4afa821895dcc9f596b404b62ca066f79bf0231b
iwana994994 <iwana994994@gmail.com>
2026-02-03 15:30:20 +0100

    fixing

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,12 +20,10 @@ app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-// middleware
-app.use(express.json());
 
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 
 
